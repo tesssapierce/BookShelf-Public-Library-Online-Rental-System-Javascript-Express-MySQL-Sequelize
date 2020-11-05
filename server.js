8868df9766c8205360388f90746ee89d0cb60bec
Casino Apte <casino.apte@gmail.com>
2020-11-05 11:53:32 -0700

    initial commit

---
+++ server.js
@@ -0,0 +1,12 @@
+var express = require("express");
+var app = express();
+var PORT = process.env.PORT || 8080;
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+var exphbs = require("express-handlebars");
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+app.listen(PORT, function () {
+    console.log("App listening on PORT " + PORT);
+})
