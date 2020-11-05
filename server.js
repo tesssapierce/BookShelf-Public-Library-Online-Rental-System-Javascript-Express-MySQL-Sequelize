e8f8a6f59a7dcda753d7a327cab6a39f3aecb29f
Tessa Pierce <tesssapierce@gmail.com>
2020-11-05 15:41:33 -0700

    get works

--- server.js
+++ server.js
@@ -1,19 +1,33 @@
+//Express
 var express = require("express");
 var app = express();
+
+//Models
 var db=require("./models");
 
+//Server BS
 var PORT = process.env.PORT || 8080;
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+//Handlebars
 var exphbs = require("express-handlebars");
-app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+const handlebarsConfig = {
+  defaultLayout: "main", 
+  runtimeOptions: {
+    allowProtoPropertiesByDefault: true,
+    allowProtoMethodsByDefault: true
+  } 
+}
+app.engine("handlebars", exphbs(handlebarsConfig));
 app.set("view engine", "handlebars");
-app.listen(PORT, function () {
-    console.log("App listening on PORT " + PORT);
-})
+
+//Routes
+require("./routes/html/htmlRoutes.js")(app);
 
 
+//Listener + Sequelize Sync
 db.sequelize.sync({ force: true }).then(function() {
     app.listen(PORT, function() {
       console.log("App listening on PORT " + PORT);
