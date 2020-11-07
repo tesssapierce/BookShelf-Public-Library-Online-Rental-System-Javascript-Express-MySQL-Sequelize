7c7febf04fe7ff633daf1e08bb8e61072c33e5b0
Beni Mahat <benimahat6565@gmail.com>
2020-11-06 20:55:16 -0700

    updated handle folders conneted homepage to localhost/

--- models/books.js
+++ models/books.js
@@ -1,3 +1,5 @@
+
+
 module.exports = function(sequelize, DataTypes) {
     var Books = sequelize.define("Books", {
       book_id: {
--- routes/html/htmlRoutes.js
+++ routes/html/htmlRoutes.js
@@ -1,21 +1,40 @@
 const { query } = require("express");
 var path = require("path");
-const db = require("../../models");
+const db = require("../../models/books.js");
 
-module.exports = function(app){
-  app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname, "../../public/assets/html/index.html"));
-  });
-  app.get("/user/:username", function(req, res) {
-    var username = req.params.username
+module.exports = function (app) {
 
-    db.User.findAll().then(function(dbUser){
-    console.log(dbUser)
-    })
-
-    // res.render("profile", {data: books})
-  });
-  app.get("/login", function(req, res){
-    res.sendFile(path.join(__dirname, "../../public/assets/html/login.html"))
+  app.get("/", function(req, res){
+    res.render("homepage-block");
   })
-};
+
+  app.get("/user/", function(req, res){
+    res.render("profile");
+  })
+
+}
+
+
+
+//   app.get("/", function (req, res) {
+//     res.render('homepage-block')
+//   )
+// };
+// // db.Books.findAll({}).then(function (allBooksData) {
+// //   console.log(allBooksData);
+// // });
+
+// app.get("/user/", function (req, res) {
+//   // var username = req.params.username
+//   // db.User.findAll().then(function (dbUser) {
+//   //   console.log(dbUser)
+//   // })
+//   // res.render("profile-block", {data: books})
+//   res.render("profile-block");
+//     )};
+
+
+//     // app.get("/login", function (req, res) {
+//     //   res.sendFile(path.join(__dirname, "../../public/assets/html/login.html"))
+//     // })
+// }
