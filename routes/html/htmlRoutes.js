4f98cfb99479cac829198e1ba6433bc4981bad9c
Andy Greenhaw <andy.greenhaw@gmail.com>
2020-11-07 23:59:41 -0700

    got the handlebats to feed

--- routes/html/htmlRoutes.js
+++ routes/html/htmlRoutes.js
@@ -1,3 +1,4 @@
+const { SSL_OP_SSLEAY_080_CLIENT_DH_BUG } = require("constants");
 const { query } = require("express");
 var path = require("path");
 const db = require("../../models");
@@ -11,10 +12,34 @@ module.exports = function(app){
   app.get("/user/:username", function(req, res) {
     var username = req.params.username
 
-    db.User.findAll({where: {username:username}}).then(function(dbUser){
-    console.log(dbUser)
+    db.User.findOne({where: {username:username}}).then(function(dbUser){
+    console.log(dbUser.dataValues)
+    console.log("ISBN: " + JSON.parse(dbUser.dataValues.books_owned))
 
-    res.render("profile", {data: dbUser})
+      // if (JSON.parse(dbUser.dataValues.books_owned) === null){
+      //   let booksOwned = ["../assets/images/bookself-placeholder.png"]
+      // } else {
+      //   let booksOwned = JSON.parse(dbUser.dataValues.books_owned)
+      // }
+
+      // if (JSON.parse(dbUser.dataValues.books_onloan) === null){
+      //   let booksOnloan = ["../assets/images/bookself-placeholder.png"]
+      // } else {
+      //   let booksOwned = JSON.parse(dbUser.dataValues.books_owned)
+      // }
+
+    let profilePage = {
+      username: dbUser.dataValues.username,
+      zipcode: dbUser.dataValues.zipcode,
+      about_me: dbUser.dataValues.about_me,
+      books_owned: JSON.parse(dbUser.dataValues.books_owned),
+      books_onloan: JSON.parse(dbUser.dataValues.books_owned)
+    }
+
+    console.log(profilePage)
+
+    res.render("profile", profilePage)
+
     })
 
   });
