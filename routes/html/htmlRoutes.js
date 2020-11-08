6238685162db22541af7b43de9be5b45bab16b2a
Andy Greenhaw <andy.greenhaw@gmail.com>
2020-11-08 03:15:10 -0700

    Profiles are all functionally working.

--- routes/html/htmlRoutes.js
+++ routes/html/htmlRoutes.js
@@ -8,32 +8,68 @@ module.exports = function(app){
     res.sendFile(path.join(__dirname, "../../public/assets/html/index.html"));
   });
   
-  
+  ///////////////////////////////////////
+  // GET ROUTE: PROFILE PAGE FORMATTER //
+  ///////////////////////////////////////
+
   app.get("/user/:username", function(req, res) {
     var username = req.params.username
 
+    // Match username with Database and make dbUser the user's data Object
     db.User.findOne({where: {username:username}}).then(function(dbUser){
     console.log(dbUser.dataValues)
     console.log("ISBN: " + JSON.parse(dbUser.dataValues.books_owned))
 
-      // if (JSON.parse(dbUser.dataValues.books_owned) === null){
-      //   let booksOwned = ["../assets/images/bookself-placeholder.png"]
-      // } else {
-      //   let booksOwned = JSON.parse(dbUser.dataValues.books_owned)
-      // }
+    // Create Array of Images for Empty Array Results
+    let emptyCover = "../assets/images/emptycover-placeholder.jpg";
+    let emptyArray = [emptyCover];
 
-      // if (JSON.parse(dbUser.dataValues.books_onloan) === null){
-      //   let booksOnloan = ["../assets/images/bookself-placeholder.png"]
-      // } else {
-      //   let booksOwned = JSON.parse(dbUser.dataValues.books_owned)
-      // }
+    // Set Arrays for Books and Cover Image Code
+    let booksOwned = [];
+    let ownedCoverImg =[];
+    let booksBorrowed = [];
+    let borrowedCoverImg = [];
 
+
+    // Format Cover Image Code
+    function formatImageCode(){      
+      console.log("Formatting image code")
+      // Loop imageSrc Code with ISBN Number
+      for(var i=0; i < booksOwned.length; i++){
+        imageSrc="http://covers.openlibrary.org/b/isbn/" + booksOwned[i] + ".jpg";
+        ownedCoverImg.push(imageSrc);
+      }
+      console.log("TEST: " + ownedCoverImg)
+    }
+
+    // If User Owns No Books, Feed Placeholder Image
+      if (dbUser.dataValues.books_owned == ""){
+        ownedCoverImg = emptyArray; 
+      //   // return booksOnloan;
+      } else {
+        booksOwned = JSON.parse(dbUser.dataValues.books_owned);
+        formatImageCode();
+      }
+
+    // If User Has No Borrowed Books, Feed Placeholder Image
+      if (dbUser.dataValues.books_onloan == ""){
+        borrowedCoverImg = emptyArray; 
+      //   // return booksOnloan;
+      } else {
+        booksOnloan = JSON.parse(dbUser.dataValues.books_owned);
+        formatImageCode();
+      }
+
+    // console.log("ONLOAN: " + dbUser.dataValues.books_onloan)
+    // console.log("ONLOAN2: " + booksOnloan)
+
+    // Reformat profilePage Object
     let profilePage = {
       username: dbUser.dataValues.username,
       zipcode: dbUser.dataValues.zipcode,
       about_me: dbUser.dataValues.about_me,
-      books_owned: JSON.parse(dbUser.dataValues.books_owned),
-      books_onloan: JSON.parse(dbUser.dataValues.books_owned)
+      books_owned: ownedCoverImg,
+      books_onloan: borrowedCoverImg
     }
 
     console.log(profilePage)
