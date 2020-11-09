d749dd5b8805f525e329f9316171ae79c9f46830
Tessa Pierce <tesssapierce@gmail.com>
2020-11-09 10:11:07 -0700

    fixing database bugs

--- migrations/20201109043709-create-books.js
+++ migrations/20201109043709-create-books.js
@@ -2,13 +2,10 @@
 module.exports = {
   up: async (queryInterface, Sequelize) => {
     await queryInterface.createTable('Books', {
-      id: {
+      book_id: {
         allowNull: false,
         autoIncrement: true,
         primaryKey: true,
-        type: Sequelize.INTEGER
-      },
-      book_id: {
         type: Sequelize.INTEGER
       },
       isbn: {
